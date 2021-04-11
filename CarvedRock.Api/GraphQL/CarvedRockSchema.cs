a379d5fcb2b6f54dcd2b4ee64c52b8a917a83236
Shenyi <baoshenyi@gmail.com>
2021-04-11 09:38:40 -0600

    added schema with serivce injection error

---
+++ CarvedRock.Api/GraphQL/CarvedRockSchema.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CarvedRock.Api.GraphQL
+{
+    public class CarvedRockSchema : Schema
+    {
+        public CarvedRockSchema(IDependencyResolver resolver): base((IServiceProvider)resolver)
+        {
+            Query = resolver.GetService<CarvedRockQuery>();
+        }
+    }
+}
--- CarvedRock.Api/Startup.cs
+++ CarvedRock.Api/Startup.cs
@@ -1,9 +1,14 @@
 ﻿using CarvedRock.Api.Data;
+using CarvedRock.Api.GraphQL;
 using CarvedRock.Api.Repositories;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Web.Mvc;
 
 namespace CarvedRock.Api
 {
@@ -18,14 +23,22 @@ namespace CarvedRock.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddDbContext<CarvedRockDbContext>(options =>
                 options.UseSqlServer(_config["ConnectionStrings:CarvedRock"]));
             services.AddSingleton<ProductRepository>();
+            services.AddScoped<CarvedRockSchema>();
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = true;
+                //options.ExposeExceptions = true;
+            }).AddGraphTypes(ServiceLifetime.Scoped);
         }
 
-        public void Configure(CarvedRockDbContext dbContext)
+        public void Configure(IApplicationBuilder app, CarvedRockDbContext dbContext)
         {
+            app.UseGraphQL<CarvedRockSchema>();
+            app.UseGraphQLPlayground();
+            //app.UseGraphQLPlayground(new PlaygroundOptions());
             dbContext.Seed();
         }
     }
